e073641929c36fd3cb7af187ced3f931614788fe
Alexander DuPree <alexander.j.dupree@gmail.com>
2021-03-29 16:33:54 -0700

    Implemented CLI for modexp program

--- modexp/src/main.rs
+++ modexp/src/main.rs
@@ -1,7 +1,28 @@
 /// Entrypoint for modexp command line utility
 
+use std::env;
+
 use modexp::modexp;
 
+
+fn error() -> ! {
+    eprintln!("modexp: usage: modexp <x> <y> <m>");
+    std::process::exit(1);
+}
+
+
 fn main() {
-    println!("Hello, world! {}\n", modexp(2, 20, 17));
+    let args: Vec<u32> = 
+        env::args()
+        .skip(1)
+        .map(|s| s.parse().unwrap_or_else(|_| error()))
+        .collect();
+
+    if args.len() != 3 {
+        error();
+    }
+
+    println!("{}", modexp(args[0], args[1], args[2]));
+
+    std::process::exit(0);
 }
